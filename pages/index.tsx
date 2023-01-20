adde2092275f912d7dbe9e769b96886b305639a4
liccabobo <licca.bobo@gmail.com>
2023-01-20 20:58:06 +0800

    feat: Add camera and keep rotating horizontally

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,71 @@
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
-import * as THREE from 'three'
-import React, { Suspense, useRef, useState } from 'react'
-import { Canvas, useFrame, ThreeElements } from '@react-three/fiber'
-import { useLoader } from '@react-three/fiber'
+import React, { Suspense, useEffect } from 'react'
+import { Canvas, useLoader, useThree } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-const dracoLoader = new DRACOLoader();
-dracoLoader.setDecoderPath('/gltf/');
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('/gltf/')
 
-const gltfLoader = new GLTFLoader();
-gltfLoader.setDRACOLoader(dracoLoader);
+const gltfLoader = new GLTFLoader()
+gltfLoader.setDRACOLoader(dracoLoader)
 
-function Scene() {
+const CameraController = () => {
+  const { camera, gl } = useThree();
+  
+  useEffect(
+    () => {
+      const controls = new OrbitControls(camera, gl.domElement)
+      controls.enablePan = false;
+      controls.position0.set(0, 0, 0);
+      
+      controls.minPolarAngle = 0.43 * Math.PI;
+      controls.maxPolarAngle = 0.45 * Math.PI;
+      
+      controls.minAzimuthAngle = -Infinity;
+      controls.maxAzimuthAngle = Infinity;
+      controls.update();
+      
+      return () => {
+        controls.dispose();
+      };
+    },
+    [camera, gl]
+  );
+  return null;
+};
+
+function Island() {
   const island = useLoader(GLTFLoader, '/island.glb', (loader) => {
     loader.setDRACOLoader(dracoLoader)
   })
 
+  return (
+    <Suspense fallback={null}>
+      <CameraController />
+      <primitive 
+        object={island.scene} 
+        scale={[0.33, 0.33, 0.33]}
+        rotation={[0, 0, 0]}
+      />
+    </Suspense>
+  )
+}
+
+function Cyclist() {
   const cyclist = useLoader(GLTFLoader, '/cyclist.glb', (loader) => {
     loader.setDRACOLoader(dracoLoader)
   })
 
-  const meshRef = useRef();
-  useFrame((state, delta) => {
-    meshRef.current.rotation.x += 0.01;
-    meshRef.current.rotation.y += 0.01;
-  })
-
   return (
     <Suspense fallback={null}>
-      <primitive object={island.scene} scale={[0.4, 0.4, 0.4]} ref={meshRef}/>
-      <primitive object={cyclist.scene} scale={[0.5, 0.5, 0.5]} />
+      <primitive 
+        object={cyclist.scene} 
+        scale={[0.1, 0.1, 0.1]} 
+        position={[0, 0, 3.8]
+      }/>
     </Suspense>
   )
 }
@@ -45,10 +79,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>      
+      <main className={styles.main}>
         <Canvas className={styles.canvas}>
           <ambientLight />
-          <Scene />
+          <Island />
+          <Cyclist />
         </Canvas>
       </main>
     </>
